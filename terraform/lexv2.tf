22dd3ddc765bda2b2beee6cd71676ca9d637bad2
Jonah Uka <jonah.uka@gmail.com>
2025-12-04 12:23:50 +0100

    Refactor lexv2.tf to improve formatting by removing unnecessary blank lines

--- terraform/lexv2.tf
+++ terraform/lexv2.tf
@@ -8,16 +8,16 @@ resource "aws_cloudformation_stack" "lex_bot_stack" {
     LexRoleArn           = aws_iam_role.lex_bot_role.arn
     LambdaFulfillmentArn = aws_lambda_function.lex_fulfillment.arn
     BotAliasName         = "LiveBotAlias"
-    
+
     # === PASSING TAGS TO CFN ===
     AssessmentTag = "SolutionsEngineer-candidate-jonah"
     ProjectTag    = "Swiftline"
     CreatedByTag  = "terraform"
 
     LexLogGroupArn = aws_cloudwatch_log_group.lex_logs.arn
-    
+
   }
-  
+
 
   # depends_on = [
   #   aws_iam_role.lex_bot_role,
